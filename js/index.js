661edb23c50c975aacfabe932989b3a720ec189c
Sandro <sandro.scalco@gmail.com>
2014-05-30 07:29:06 -0400

    neuer button

--- js/index.js
+++ js/index.js
@@ -29,6 +29,7 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.getElementById('scan').addEventListener('click', this.scan, false);
         document.getElementById('encode').addEventListener('click', this.encode, false);
+        document.getElementById('btnscan').addEventListener('click', this.encode, false);
     },
 
     // deviceready Event Handler
